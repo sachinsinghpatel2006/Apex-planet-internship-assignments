925bafc812303ec377a80a703ecb062ddf8c42f6
Sachin singh patel <sachinsinghpatel343@gmail.com>
2025-12-19 20:07:13 +0530

    Add files via upload

---
+++ Task 3/script.js
@@ -0,0 +1,5 @@
+src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"
+var typed = new Typed('#element', {
+    strings: ['C++ Programmer','UI /UX Designer','DSA & Problem-Solving','Web Developer'],
+    typeSpeed: 45,
+});
